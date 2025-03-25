5df20ccd7bc5eb0bdaf476ce78a0d6bc36776ad7
parmarkrishnaa <parmarkrishna050611@gmail.com>
2025-03-25 19:35:28 +0530

    operators

---
+++ DSA-C++/arith.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+
+    cout << "Enter num1: ";
+    cin >> num1;
+
+    cout << "Enter num2: ";
+    cin >> num2;
+
+    cout << "num1 + num2 = " << num1 + num2 << endl;
+    
+    cout << "num1 - num2 = " << num1 - num2 << endl;
+    
+    cout << "num1 * num2 = " << num1 * num2 << endl;
+    
+    cout << "num1 / num2 = " << num1 / num2 << endl;
+    
+    cout << "num1 % num2 = " << num1 % num2 << endl;
+
+    return 0;
+}
